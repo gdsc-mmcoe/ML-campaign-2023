acee28399e54dd749906ef3b7a506b7eaee3699c
atharvabhide <atharvabhide91@gmail.com>
2023-04-10 01:47:32 +0530

    add python basics notebook and utility function

---
+++ add_subtract_module.py
@@ -0,0 +1,23 @@
+def add_two_numbers(a : int, b : int) -> int:
+    """Adds two integers together.
+
+    Args:
+        a (int): first integer
+        b (int): second integer
+
+    Returns:
+        int: addition of the two integers a and b
+    """
+    return a + b
+
+def subtract_two_numbers(a : int, b : int) -> int:
+    """Subtracts two integers.
+
+    Args:
+        a (int): first integer
+        b (int): second integer
+
+    Returns:
+        int: subtraction of the two integers a and b
+    """
+    return a - b
